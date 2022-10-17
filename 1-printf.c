c5750ca588528d548798b43d637554f62e88537c
SiBen10 <benjaminkn805@gmail.com>
2022-10-17 09:37:42 +0100

    Collaboration work on print functions

--- 1-printf.c
+++ 1-printf.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _printf - prints and input into the standard output
+ * _printf - functions to prints and input into the standard output
  * @format: the format string
  * Return: number of bytes printed
  */
@@ -10,12 +10,12 @@ int _printf(const char *format, ...)
 
 {
 	int sum = 0;
-	va_list ap;
+	va_list y;
 	char *p, *start;
 
-	params_t params = PARAMS_INIT;
+	z_t z = Z_INIT;
 
-	va_start(ap, format);
+	va_start(y, format);
 
 	if (!format || (format[0] == '%' && !format[1]))/* checking for NULL char */
 		return (-1);
@@ -23,7 +23,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
-		init_params(&params, ap);
+		init_z(&z, y);
 		if (*p != '%')/*checking for the % specifier*/
 		{
 			sum += _putchar(*p);
@@ -31,21 +31,21 @@ int _printf(const char *format, ...)
 		}
 		start = p;
 		p++;
-		while (get_flag(p, &params)) /* while char at p is flag character */
+		while (get_flag(p, &z)) /* initializing while loop */
 		{
-			p++; /* next character */
+			p++; 
 		}
-		p = get_width(p, &params, ap);
-		p = get_precision(p, &params, ap);
-		if (get_modifier(p, &params))
+		p = get_width(p, &z, y);
+		p = get_precision(p, &z, y);
+		if (get_modifier(p, &z))
 			p++;
 		if (!get_specifier(p))
 			sum += print_from_to(start, p,
-					params.l_modifier || params.h_modifier ? p - 1 : 0);
+					z.l_modifier || z.h_modifier ? p - 1 : 0);
 		else
-			sum += get_print_func(p, ap, &params);
+			sum += get_print_func(p, y, &z);
 	}
 	_putchar(BUF_FLUSH);
-	va_end(ap);
+	va_end(y);
 	return (sum);
 }
