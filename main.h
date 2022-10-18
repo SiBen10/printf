fabdd09feb62249b0df40c6f75f26e8679b85d8f
Lawrence Elejoka <111017669+Pipiwale@users.noreply.github.com>
2022-10-18 13:43:15 +0100

    Update main.h

--- main.h
+++ main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -23,8 +24,24 @@ int _printf(const char *format, ...);
 
 int _putchar(char c);
 
+int print_uns_num(unsigned int n);
+
+int hex_check(int num, char case_type);
+
 int p_char(va_list args);
 
 int p_str(va_list args);
+
+int p_nums(va_list args);
+
+int dec_to_bin(va_list args);
+
+int p_uns_int(va_list args);
+
+int dec_to_oct(va_list args);
+
+int dec_to_hex(va_list args);
+
+int dec_to_HEX(va_list args);
 
 #endif
